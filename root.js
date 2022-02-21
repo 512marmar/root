e029ba8dad44dfa3163d63ff93506e59c1587372
student11 <student11@gmail.com>
2022-02-21 19:04:08 +0200

    first commit

---
+++ root.js
@@ -0,0 +1,33 @@
+let inputWidth = document.querySelector (".inputwidth");
+let inputHeight = document.querySelector (".inputheight");
+let inputColor = document.querySelector (".inputcolor");
+let btnCreate = document.querySelector (".input_create");
+
+
+let newDiv;
+
+let iwValue;
+let ihValue;;
+let icValue;
+
+let isDiv = false;
+
+function createDiv() {
+    if (!isDiv) {
+        iwValue = inputWidth.value;
+        ihValue = inputHeight.vaule;
+        icValue = inputColor.vaule;
+
+    if (iwValue >= 50 && ihValue >= 50 && iwValue <= 500 && ihValue <= 500) {
+        newDiv = document.createElement("div");
+        newDiv.style.width = iwValue + "px";
+        newDiv.style.height = ihValue + "px";
+        newDiv.style.background = icValue
+        
+    
+        document.querySelector("containet").prepend(newDiv);
+        inputHeight.setAttribute("readonly", 1);
+        inputWidth.setAttribute ("readopnly", 1);
+    }   
+    }
+}
